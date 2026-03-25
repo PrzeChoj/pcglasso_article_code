27df9c35b08cd16b416f9e3015a99ff06881c12f
PrzeChoj <premysl.choj@gmail.com>
2026-03-25 12:18:41 +0100

    Appendix A, delete p=10 and delete all results

--- experiments/Appendix_A/0_parameters.R
+++ experiments/Appendix_A/0_parameters.R
@@ -1,5 +1,5 @@
 M <- 100
-p_vec <- c(10, 50, 100, 150, 200)
+p_vec <- c(50, 100, 150, 200)
 lambda_vec <- c(0.1, 0.2)
 alpha_vec <- c(0, 0.5)
 tolerance_list <- exp(seq(log(0.01), log(0.00000001), length.out = 12))
