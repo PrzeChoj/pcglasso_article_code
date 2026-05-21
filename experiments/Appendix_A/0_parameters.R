cb69c9835dda832f5d98ca3acbeb5b7051d64468
PrzeChoj <premysl.choj@gmail.com>
2026-05-21 11:12:41 +0200

    Appendix A: make the experiment smaller

--- experiments/Appendix_A/0_parameters.R
+++ experiments/Appendix_A/0_parameters.R
@@ -1,8 +1,10 @@
 M <- 2
-p_vec <- c(50, 100, 150, 200)
+#p_vec <- c(50, 100, 150, 200)
+p_vec <- c(50, 100, 150)
 lambda_vec <- c(0.1, 0.2)
 alpha_vec <- c(0, 0.5)
-tolerance_list <- exp(seq(log(0.01), log(0.00000001), length.out = 12))
+#tolerance_list <- exp(seq(log(0.01), log(0.00000001), length.out = 12))
+tolerance_list <- exp(seq(log(0.01), log(0.00001), length.out = 12))
 graph_structure_vec <- c("hub_1", "hub_09", "AR2", "random")
 #solver_vec <- c("pcglassoFast_Primal", "pcglassoFast_Dual", "pcglassoFast_PrimalDual", "pcglasso", "path_up", "path_down")
 solver_vec <- c("pcglassoFast_Primal", "pcglassoFast_Dual", "pcglasso", "path_up", "path_down")
