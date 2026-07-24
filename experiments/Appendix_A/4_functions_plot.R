64c3f05de3880bca7b3abba1fe6d56b54ab9a65e
PrzeChoj <premysl.choj@gmail.com>
2026-07-25 00:18:17 +0200

    better legend for violin

--- experiments/Appendix_A/4_functions_plot.R
+++ experiments/Appendix_A/4_functions_plot.R
@@ -9,12 +9,7 @@ brighten <- function(cols, amount = 0.45) {
   grDevices::rgb(rgb2[1, ], rgb2[2, ], rgb2[3, ], maxColorValue = 255)
 }
 
-make_matrix_legend <- function() {
-  solvers <- c("pcglasso", "pcglassoFast_Dual", "pcglassoFast_Primal", "pcglassoFast_PrimalDual", "path_up", "path_down")
-
-  base_cols <- setNames(scales::hue_pal()(length(solvers)), solvers)
-  bright_cols <- setNames(brighten(base_cols, amount = 0.45), solvers)
-
+make_matrix_legend <- function(solvers, base_cols, bright_cols) {
   legend_df <- tibble::tibble(
     solver = factor(solvers, levels = solvers),
     C = unname(base_cols[solvers]),
@@ -23,30 +18,21 @@ make_matrix_legend <- function() {
     tidyr::pivot_longer(c(C, I), names_to = "start", values_to = "col") %>%
     mutate(start = factor(start, levels = c("C", "I")))
 
-  ggplot(legend_df, aes(x = solver, y = start)) +
-    geom_tile(aes(fill = col),
-      width = 0.85, height = 0.85,
-      color = "grey40", linewidth = 0.3
+  ggplot(legend_df, aes(x = start, y = solver)) +
+    geom_point(aes(fill = col),
+      shape = 22, size = 14,
+      color = "grey40", stroke = 0.4
     ) +
     scale_fill_identity() +
-    scale_y_discrete(limits = c("I", "C"), expand = expansion(mult = c(0, 0))) +
-    scale_x_discrete(expand = expansion(mult = c(0.05, 0.05))) +
-    coord_fixed(ratio = 1, clip = "off") +
-    annotate("text",
-      x = 2.5, y = 2.65, label = "starting point / algorithm",
-      size = 4, hjust = 0.5
-    ) +
-    expand_limits(y = 2.8) +
-    annotate("text", x = 1, y = 0.05, label = "pcglasso", angle = 90, size = 4) +
-    annotate("text", x = 2, y = -0.5, label = "pcglassoFast_Dual", angle = 90, size = 4) +
-    annotate("text", x = 3, y = -0.6, label = "pcglassoFast_Primal", angle = 90, size = 4) +
-    annotate("text", x = 4, y = -0.8, label = "pcglassoFast_PrimalDual", angle = 90, size = 4) +
-    expand_limits(y = 0.35) +
+    scale_x_discrete(limits = c("C", "I"), expand = expansion(add = 0.5)) +
+    scale_y_discrete(limits = rev(solvers), expand = expansion(add = 0.7)) +
+    labs(title = "algorithm / starting point") +
     theme_void(base_size = 11) +
     theme(
-      axis.text.x = element_blank(),
-      plot.margin = margin(0, 0, 0, 0),
-      axis.text.y = element_text(margin = margin(r = 2), size = 12)
+      plot.title = element_text(size = 12, hjust = 0.5, margin = margin(b = -4)),
+      axis.text.x = element_text(size = 12),
+      axis.text.y = element_text(margin = margin(r = 3), size = 10),
+      plot.margin = margin(0, 5, 0, 5)
     )
 }
 
@@ -303,7 +289,9 @@ make_violin_time_vs_p_plot <- function(df_sub, K_val) {
       strip.background = element_rect(fill = "grey92")
     )
 
-  my_legend <- make_matrix_legend()
+  my_legend <- make_matrix_legend(sol_levels, base_cols, bright_cols)
+  legend_panel <- plot_spacer() / my_legend / plot_spacer() +
+    plot_layout(heights = c(1, 1.5, 1))
 
-  plt_main + my_legend + plot_layout(widths = c(4.7, 1))
+  plt_main + legend_panel + plot_layout(widths = c(4.7, 1))
 }
